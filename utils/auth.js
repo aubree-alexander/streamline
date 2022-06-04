db0f784d8eac480553057e18b9765163235021db
Aubree Alexander <aubree.alexander@gmail.com>
2022-06-04 17:15:04 -0500

    add withAuth function in auth.js file to determine if users are logged in

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,4 @@
+// withAuth determines if someone is logged in. if not, redirect them to login page
 const withAuth = (req, res, next) => {
     if (!req.session.userId) {
       res.redirect("/login");
