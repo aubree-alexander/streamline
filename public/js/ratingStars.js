682cc7ab7665819caf112ef0a0d60479eb97d87b
0ffpudding <jennaanderson00@gmail.com>
2022-06-06 17:10:06 -0500

    created js file to handle populating rating stars in entry and carousel

---
+++ public/js/ratingStars.js
@@ -0,0 +1,4 @@
+// populate star icons in entries based on rating entered by user
+// applies to userEntry and homepage
+// populate stars under rating selection in userEntry
+// populate stars above description in homepage carousel cards
