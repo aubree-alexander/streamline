32334a27cc7d489ecf936edf08bd34fcacb821d3
Aubree Alexander <aubree.alexander@gmail.com>
2022-06-06 18:39:34 -0500

    add loginFormHandler function for login functionality

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginFormHandler = async function(event) {
+    event.preventDefault();
+
+    const usernameEl = document.querySelector('#login-username');
+    const passwordEl = document.querySelector('#login-password');
+
+    const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameEl.value,
+            password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/homepage');
+    } else {
+        alert('Failed to login.');
+    }
+};
+
+//AA - we will need to make sure id lines up with what Jenna created in the views folder.
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
