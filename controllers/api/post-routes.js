29720f714588ad3553f957319e6b752fc793903c
smckelli <smckelli@gmail.com>
2022-06-06 09:20:56 -0500

    cleaning the slate on the api folder--commenting everyting out---

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,38 +1,56 @@
 const router = require('express').Router();
-const { MovieShow } = require('../../models/');
+const { MovieShow, StreamingService, User } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
 //post new movie or show
 //AA - we need to link data from new post form here
-router.post('/', withAuth, async (req, res) => {
-  const body = req.body;
+// router.post('/', withAuth, async (req, res) => {
+//   const body = req.body;
 
-  try {
-    const newMovieShow = await MovieShow.create({ ...body, userId: req.session.userId });
-    res.json(newMovieShow);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//   try {
+//     const newMovieShow = await MovieShow.create({ ...body, userId: req.session.userId });
+//     res.json(newMovieShow);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// SAM- linking 'GET' data to 'MovieShow' in models
+// Get all posts
+// router.get("/", (req, res) => {
+//   Post.findAll({
+//       attributes: ["id", "title", "yearReleased", "streamingservice_id"],
+//       include: [{
+//               model: MovieShow,
+//               attributes: ["username"],
+//           },
+//       ],
+//     })
+//     .then((dbPostData) => res.json(dbPostData))
+//     .catch((err) => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
 
 //update existing movie or show
-router.put('/:id', withAuth, async (req, res) => {
-  try {
-    const [affectedRows] = await MovieShow.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
+// router.put('/:id', withAuth, async (req, res) => {
+//   try {
+//     const [affectedRows] = await MovieShow.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
 
-    if (affectedRows > 0) {
-      res.status(200).end();
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     if (affectedRows > 0) {
+//       res.status(200).end();
+//     } else {
+//       res.status(404).end();
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 //AA - keeping this here in case we want to let people delete posts.
 // router.delete('/:id', withAuth, async (req, res) => {
