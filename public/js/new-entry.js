846aedff99c07ebb3c713327bd5fa2cbfe213564
Aubree Alexander <aubree.alexander@gmail.com>
2022-06-08 17:27:46 -0500

    move cloudinary js to designiated new-entry js file

--- public/js/new-entry.js
+++ public/js/new-entry.js
@@ -1,3 +1,17 @@
+const uploadUrlInput = document.querySelector('[name="upload_url"]')
+
+const uploadWidgetButton = document.getElementById("upload_widget")
+const myWidget = cloudinary.createUploadWidget({
+    cloudName: 'ddmkrf5bx123', 
+    uploadPreset: 's4wgstbl'
+}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+        console.log('Done! Here is the image info: ', result.info); 
+        uploadUrlInput.value = result.info.secure_url
+        uploadWidgetButton.style.display = 'none'
+    }
+})
+
 const newEntryHandler = async function(event) {
     event.preventDefault();
 
@@ -16,6 +30,10 @@ const newEntryHandler = async function(event) {
     document.location.replace('/homepage');
 
 };
+
+uploadWidgetButton.addEventListener('click', function(){
+myWidget.open();
+}, false);
 
 //AA - make sure id lines up with what Jenna has
 document.querySelector('#new-post-form').addEventListener('submit', newEntryHandler);
