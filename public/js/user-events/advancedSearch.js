6d0434e0ceaaffa3b9cbef5d819ebda8c6a8a72b
Aubree Alexander <aubree.alexander@gmail.com>
2022-06-08 12:48:06 -0500

    remove user-events js file; will be writing those routes in controllers folder

--- public/js/user-events/advancedSearch.js
+++
@@ -1,9 +0,0 @@
-//AA - this isn't complete - need to populate handebars template 
-const advancedSearch = async function(event) {
-    event.preventDefault();
-
-    document.location.replace('/advancedSearch')
-};
-
-
-document.querySelector('#advancedSearchBtn').addEventListener('click', advancedSearch)
